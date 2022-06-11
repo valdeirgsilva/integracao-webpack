e32635d29a550fb6cdd94185dff3726d6dff96b9
Valdeir Gabriel da Silva <valdeirgabriels@gmail.com>
2022-06-11 20:59:09 -0300

    Configurando o Webpack Dev Server

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,4 @@ const livro = new Livro('Dom Quixote', 108.80, 0.10);
 // console.log(livro.precoComDesconto());
 
 $('body').append(`<h1>${livro.nome}</h1>`);
-$('body').append(`<h2>Preço: R$${livro.precoComDesconto()}</h2>`);
+$('body').append(`<h2>R$${livro.precoComDesconto()}</h2>`);
